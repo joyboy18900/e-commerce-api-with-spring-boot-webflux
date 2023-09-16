bbd5af7b10e13adc87a9a646742b45cd6093d169
joyboy18900 <surakiat.dev@gmail.com>
2023-09-16 22:35:10 +0700

    add service category of product

---
+++ src/main/java/com/example/springwebfluxapi/productCategories/ProductCategory.java
@@ -0,0 +1,67 @@
+package com.example.springwebfluxapi.productCategories;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Table("product_categories")
+public class ProductCategory {
+
+    @Id
+    private Integer id;
+
+    @Column("name")
+    private String name;
+
+    @Column("description")
+    private String description;
+
+    @Column("create_date")
+    private LocalDateTime createDate;
+
+    @Column("update_date")
+    private LocalDateTime updateDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+}
