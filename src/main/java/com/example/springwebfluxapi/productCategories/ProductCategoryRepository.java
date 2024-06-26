d2784cb43fe92a720b9da6c47f0d68a8dc9af200
joyboy18900 <surakiat.dev@gmail.com>
2024-06-26 23:43:18 +0700

    Add product category repository

---
+++ src/main/java/com/example/springwebfluxapi/productCategories/ProductCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.springwebfluxapi.productCategories;
+
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductCategoryRepository extends R2dbcRepository<ProductCategory, Integer> {
+}
